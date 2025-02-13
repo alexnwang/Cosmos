7b585142022001877f4e42fba81a4a4b5c0d3eaa
alex wang <anw2067@nyu.edu>
2025-02-13 20:23:07 +0000

    multiprocessing

--- action_anticipation/diffusion_data_prep/ek100_from_rgb.py
+++ action_anticipation/diffusion_data_prep/ek100_from_rgb.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+from functools import partial
 import glob
 import json
 import os
@@ -16,6 +17,7 @@ from tqdm import tqdm
 from transformers import T5EncoderModel, T5TokenizerFast
 
 from cosmos1.utils import log
+from multiprocessing import Pool
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Process some configurations.")
@@ -33,23 +35,24 @@ def get_parser():
     parser.add_argument("--max_text_length", type=int, default=512, help="Maximum text length")
     return parser
 
-def init_t5():
+def init_t5(device="cuda"):
     """Initialize and return the T5 tokenizer and text encoder."""
     tokenizer = T5TokenizerFast.from_pretrained("google-t5/t5-11b")
     text_encoder = T5EncoderModel.from_pretrained("google-t5/t5-11b")
-    text_encoder.to("cuda")
+    text_encoder.to(device)
     text_encoder.eval()
     return tokenizer, text_encoder
 
 
-def init_video_tokenizer(tokenizer_dir: str):
+def init_video_tokenizer(tokenizer_dir: str, device="cuda"):
     """Initialize and return the Cosmos Video tokenizer."""
     dit_config = DiT7BConfig(vae_path=tokenizer_dir)
     vae = dit_config.configure_vae()
+    vae.to(device)
     return vae
 
 @torch.no_grad()
-def encode_for_batch(tokenizer, encoder, prompts: list[str], max_length=512):
+def encode_for_batch(tokenizer, encoder, prompts: list[str], max_length=512, device="cuda"):
     """
     Encode a batch of text prompts to a batch of T5 embeddings.
     Parameters:
@@ -70,8 +73,8 @@ def encode_for_batch(tokenizer, encoder, prompts: list[str], max_length=512):
     )
 
     # We expect all the processing is done on GPU.
-    input_ids = batch_encoding.input_ids.cuda()
-    attn_mask = batch_encoding.attention_mask.cuda()
+    input_ids = batch_encoding.input_ids.to(device)
+    attn_mask = batch_encoding.attention_mask.to(device)
 
     outputs = encoder(input_ids=input_ids, attention_mask=attn_mask)
     encoded_text = outputs.last_hidden_state
@@ -82,20 +85,32 @@ def encode_for_batch(tokenizer, encoder, prompts: list[str], max_length=512):
 
     return encoded_text
 
+def load_frame(video_dir, frame_number):
+    frame_path = os.path.join(video_dir, f"frame_{int(frame_number):010d}.jpg")
+    if os.path.exists(frame_path):
+        return torchvision.io.read_image(frame_path)
+    else:
+        return None
 
 def main(args):
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    rank = int(os.environ.get("RANK", 0))
+    
+    device = torch.device(f"cuda:{rank}")
+    torch.cuda.set_device(device)
+    
     # Set up output directory
     os.makedirs(args.output_path, exist_ok=True)
 
     # Initialize T5
     print("Initializing T5")
-    tokenizer, text_encoder = init_t5()
+    tokenizer, text_encoder = init_t5(device)
 
     # Initialize the VAE
     print("Initializing VAE")
     if args.tokenizer_dir == "":
         args.tokenizer_dir = snapshot_download("nvidia/Cosmos-1.0-Tokenizer-CV8x8x8")
-    vae = init_video_tokenizer(args.tokenizer_dir)
+    vae = init_video_tokenizer(args.tokenizer_dir, device)
     
     # setting constants
     t5_embeding_max_length = args.max_text_length
@@ -120,14 +135,18 @@ def main(args):
     video_info = pd.read_csv(fp)
     
     count = 0  # File index
-    with torch.no_grad():
+    with torch.no_grad(), Pool(processes=16) as pool:
         # for each action, read the start time and end time, start frame and end frame
         # train_data is a pandas dataframe
-        for idx, (_, row) in enumerate(tqdm(train_data.iterrows(), total=len(train_data))):
+        for count, (_, row) in enumerate(tqdm(train_data.iterrows(), total=len(train_data))):
+            if count % world_size != rank:
+                continue
+            
             participant_id = row['participant_id']
             video_id = row['video_id']
             if video_id in ["P01_109", "P27_103"]:
                 continue
+            
             
             fps = float(video_info[video_info['video_id'] == video_id]['fps'].values[0])
             
@@ -150,19 +169,21 @@ def main(args):
             
             # if len(frame_indices) > args.num_frames, we have more than 1 candidate start point
             # lets pick one every 2 seconds
-            for index in range(0, len(frame_numbers)-args.num_frames+1, args.fps):
-                chunk = []
-                for frame_number in frame_numbers[index:index+args.num_frames]:
-                    frame_path = os.path.join(video_dir, f"frame_{int(frame_number):010d}.jpg")
-                    chunk.append(torchvision.io.read_image(frame_path))
-                chunk = torch.stack(chunk) # (T, C, H, W)
-                
-                # save video
-                if index == 0 and idx % 5 == 0:
-                    save_path = os.path.join(args.output_path, f"{count}-{video_id}-{narration}-{frame_numbers[index]}.mp4")
-                    torchvision.io.write_video(save_path, chunk.permute(0, 2, 3, 1), args.fps)
+            for inner_count, index in enumerate(range(0, len(frame_numbers)-args.num_frames+1, args.fps)):
+                chunk = pool.map(partial(load_frame, video_dir), frame_numbers[index:index+args.num_frames])
+
+                try:
+                    chunk = torch.stack(chunk)  # (T, C, H, W)
+                except Exception as e:
+                    print(f"Skipping chunk due to error: {e}")
+                    continue
                 
                 T, C, H, W = chunk.shape
+                
+                # save video
+                if index == 0 and count % 100 == 0:
+                    save_path = os.path.join(args.output_path, f"{count}-{inner_count}-{video_id}-{narration}-{frame_numbers[index]}.mp4")
+                    torchvision.io.write_video(save_path, chunk.permute(0, 2, 3, 1), args.fps)
 
                 # if chunk_duration != vae.video_vae.pixel_chunk_duration, pad it up to the required length
                 if args.num_frames != vae.video_vae.pixel_chunk_duration:
@@ -175,7 +196,7 @@ def main(args):
                 chunk = rearrange(chunk, "(b t) c h w -> b c t h w", b=1)
                 
                 # Convert to bf16 and normalize from [0, 255] to [-1, 1]
-                chunk = chunk.to(device="cuda", dtype=torch.bfloat16, non_blocking=True) / 127.5 - 1.0
+                chunk = chunk.to(device=device, dtype=torch.bfloat16, non_blocking=True) / 127.5 - 1.0
 
                 # Encode video
                 latent = vae.encode(chunk).cpu()  # shape: (1, latent_channels, T//factor, H//factor, W//factor)
@@ -185,7 +206,7 @@ def main(args):
                     latent = latent[:, :, :(chunk_duration-1) // 8 + 1, :, :] # hack as it's always temporal stride = 8
 
                 # Encode text
-                out = encode_for_batch(tokenizer, text_encoder, [narration], max_length=t5_embeding_max_length)[0]
+                out = encode_for_batch(tokenizer, text_encoder, [narration], max_length=t5_embeding_max_length, device=device)[0]
                 encoded_text = torch.tensor(out, dtype=torch.bfloat16)
 
                 # Pad T5 embedding to t5_embeding_max_length
@@ -194,12 +215,12 @@ def main(args):
                 t5_embed[0, :L] = encoded_text
 
                 # Save data to folder
-                torch.save(latent[0], os.path.join(args.output_path, f"{count}.video_latent.pth"))
-                torch.save(t5_embed[0], os.path.join(args.output_path, f"{count}.t5_text_embeddings.pth"))
+                torch.save(latent[0], os.path.join(args.output_path, f"{count}-{inner_count}.video_latent.pth"))
+                torch.save(t5_embed[0], os.path.join(args.output_path, f"{count}-{inner_count}.t5_text_embeddings.pth"))
 
                 # Create a T5 text mask of all ones
                 torch.save(
-                    torch.ones(t5_embeding_max_length, dtype=torch.bfloat16), os.path.join(args.output_path, f"{count}.t5_text_mask.pth")
+                    torch.ones(t5_embeding_max_length, dtype=torch.bfloat16), os.path.join(args.output_path, f"{count}-{inner_count}.t5_text_mask.pth")
                 )
                 
                 # Save metadata
@@ -208,13 +229,11 @@ def main(args):
                     "width": args.width,
                     "fps": args.fps,
                     "num_frames": chunk_duration,
-                    # "video_path": video_dir,
-                    # "start_frame": start_idx,
+                    "video_path": video_dir,
+                    "start_frame": int(frame_numbers[index]),
                 }
-                with open(os.path.join(args.output_path, f"{count}.info.json"), "w") as json_file:
+                with open(os.path.join(args.output_path, f"{count}-{inner_count}.info.json"), "w") as json_file:
                     json.dump(info, json_file)
-
-                count += 1
 
                 
                 
